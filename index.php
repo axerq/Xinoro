193ff8796300955ffce13adaa4ff7a0fa971330d
axerq <94172535+axerq@users.noreply.github.com>
2022-01-27 15:13:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	ini_set('error_reporting', E_ALL);
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
-
+fweigefigiwgfigqcifuwfgytbqtvdrqrvuyqtriqytdriuq	trqgrjhafghasgigri3yg3uy3fuqidfajsdjasdjwdfjwFDJASFjvcHCJGFHFASLKHFakcbkyetwitieytvitriueruwecuy
 	# import modules
 	require_once("core/core/router.php");
 	require_once("core/core/model.php");
